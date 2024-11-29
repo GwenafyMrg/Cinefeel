d8d1867641c9b24bb62a8fa7909241e667f1bcdf
Gwenafy <gwenafy@gmail.com>
2024-11-29 17:49:51 +0100

    Displaying informations from DB to movieCards.

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,77 @@
+//-----------------------------------Importation des modules node:---------------------------------------//
+
 const express = require('express');
 const {engine} = require('express-handlebars');
+const {Sequelize, DataTypes} = require('sequelize');
 
 const path = require('path');
 
-//Définitionn de l'application Web :
+//-----------------------------------Définition du serveur BDD (mariaDB) :-------------------------------//
+const sequelize = new Sequelize('cinefeel','test','test', {
+    dialect: 'mysql',
+    host : 'localhost'
+});
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("La connexion avec la BDD est établie");
+    })
+    .catch(() => {
+        console.error("La connexion a échouée...");
+});
+
+const Movie = sequelize.define("movie", {
+    id_movie : {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement : true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    poster_url: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    runtime: {
+        type: DataTypes.SMALLINT,
+        allowNull: false
+    },
+    released_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+    },
+    avg_note: {
+        type: DataTypes.DOUBLE,
+        allowNull: false
+    }
+}, {
+        tableName: 'movie',  
+        timestamps: false,    // Utilise les champs `createdAt` et `updatedAt`.
+});
+
+sequelize.sync({force: false})
+    .then(() => {
+        console.log("Synchronisée.");
+    })
+    .catch((err) => {
+        console.error("Erreur : ", err);
+});
+
+//-----------------------------------Définitionn de l'application Web :-------------------------------//
+
 const app = express();
 app.engine('handlebars', engine({
     defaultLayout : "main",
     layoutsDir : "views/layouts",
-    partialsDir : "views/partials"
+    partialsDir : "views/partials",
+    runtimeOptions: {
+        //permet à Handlebars d'accéder aux propriétés des objets hérités (comme les instances Sequelize).
+        allowProtoPropertiesByDefault: true,
+        //permet l'accès aux méthodes du prototype
+        allowProtoMethodsByDefault: true
+    }
 }));
 
 app.set("view engine", "handlebars");
@@ -17,55 +80,14 @@ app.set("views", "./views");
 app.use(express.static(path.join(__dirname, "assets")));
 
 //Définiton des routes :
-app.get("/", (req, res) => {
-    const movies = [
-        {
-            titre : "Spider-man into the spiderverse",
-            image : "images/interstellar.jpg",
-            genres : "Superhéros - Action - Si-Fi",
-            emotions : "enjouée - excitée - curieux",
-            realisateur : "guys",
-            runtime : 120,
-            releasedDate : 2012
-        },
-        {
-            titre : "Avengers",
-            image : "images/interstellar.jpg",
-            genres : "Superhéros - Action - Si-Fi",
-            emotions : "enjouée - excitée - curieux",
-            realisateur : "guys",
-            runtime : 120,
-            releasedDate : 2012
-        },
-        {
-            titre : "Avengers",
-            image : "images/interstellar.jpg",
-            genres : "Superhéros - Action - Si-Fi",
-            emotions : "enjouée - excitée - curieux",
-            realisateur : "guys",
-            runtime : 120,
-            releasedDate : 2012
-        },
-        {
-            titre : "Avengers",
-            image : "images/interstellar.jpg",
-            genres : "Superhéros - Action - Si-Fi",
-            emotions : "enjouée - excitée - curieux",
-            realisateur : "guys",
-            runtime : 120,
-            releasedDate : 2012
-        },{
-            titre : "Avengers",
-            image : "images/interstellar.jpg",
-            genres : "Superhéros - Action - Si-Fi",
-            emotions : "enjouée - excitée - curieux",
-            realisateur : "guys",
-            runtime : 120,
-            releasedDate : 2012
-        }
-    ]
-    console.log(movies);
-    res.render("home", {movies});
+app.get("/", async (req, res) => {
+    try {
+        query = await Movie.findAll();
+        res.render("home", {movies : query});
+    } catch (err){
+        console.error("Erreur lors de l'affichage des donneés.");
+        res.status(500).send("Erreur lors de l'affichage des données.");
+    }
 });
 
 app.get("/moviesList", (req, res) => {
