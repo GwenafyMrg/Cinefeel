92de03de8e46c6985fb540d101bc4c3b1f7f57ec
Gwenafy <gwenafy@gmail.com>
2025-01-05 15:58:28 +0100

    Managing create account, login and logout page.

--- src/app.js
+++ src/app.js
@@ -5,14 +5,15 @@ const express = require('express');                         //Module express
 const {engine} = require('express-handlebars');             //Gestion d'express et handlebars
 const {Op} = require('sequelize');                          //Gestion des clauses de requête SQL
 const bodyParser = require('body-parser');                  //Gestion des entrées de formulaire HTML
-const path = require('path');                               //Gestion correctes des chemins d'accès
+const path = require('path');                               //Gestion correcte de chemins d'accès
+const session = require('express-session');                 //Gestion de sessions utilisateurs
 
 //Modules de sécurité.
 const sanitizeHtml = require('sanitize-html');              //Protéger contre les Injection SQL
 const bcrypt = require('bcrypt');                           //Hacher les mots de passe  
 
 //Import de module locaux.
-const {Movie, Director, Genre} = require('./models');       //Importation des modèles de BDD
+const {Movie, Director, Genre, User} = require('./models'); //Importation des modèles de BDD
 const funct = require('./assets/functions');                //Importer les fonctions personnelles
 
 //-----------------------------------Définition de l'application Web :-------------------------------//
@@ -46,6 +47,15 @@ app.set("views", "./views");                                //Configuration du r
 
 app.use(express.static(path.join(__dirname, "assets")));    //Définir le chemin des ressources statiques.
 app.use(bodyParser.urlencoded({ extended: true}));          //Triater les données passées par URL.
+app.use(session({
+    secret: 'io8948Yhdid00nidz', // Une clé secrète pour signer la session
+    resave: false, // Ne sauvegarde pas la session si aucune modification
+    saveUninitialized: false, // Ne crée pas une session vide
+    cookie: { 
+      maxAge: 1000*60*60, // Durée de vie du cookie (en ms)
+      httpOnly: true // Rend le cookie inaccessible depuis JavaScript côté client (sécurité+)
+    }
+}));
 
 //-----------------------------------Définitions des routes:-------------------------------//
 
@@ -66,7 +76,12 @@ app.get("/", async (req, res) => {              //Chemin d'origine
             }]
         });
         // console.log(query);
-        res.render("home", {movies : query});   //Retourner le template home.handlebars avec la donnée movies.
+        if(req.session.user){
+            res.render("home", {userData : req.session.user, movies : query});
+        }
+        else{
+            res.render("home", {movies : query});   //Retourner le template home.handlebars avec la donnée movies.
+        }
     } catch (err){
         console.error("Erreur lors de l'affichage des donneés.");
         res.status(500).send("Erreur lors de l'affichage des données.");
@@ -76,7 +91,12 @@ app.get("/", async (req, res) => {              //Chemin d'origine
 app.get("/moviesList", async (req, res) => {    //Chemin du filtrage des films obtenu par requête GET. 
 
     const genresList = await Genre.findAll();   //Récupérer tous les genres sans filtrage;
-    res.render("filter", { genresList : genresList});
+    if(req.session.user){
+        res.render("filter", {userData : req.session.user, genresList : genresList});
+    }
+    else{
+        res.render("filter", { genresList : genresList});
+    }
 });
 
 app.post("/moviesList", async (req,res) => {    //Chemin du filtrage des films obtenu par requête POST.
@@ -106,11 +126,22 @@ app.post("/moviesList", async (req,res) => {    //Chemin du filtrage des films o
     });
     // console.log(queryFilter);
 
-    res.render("filter", {genresList : await Genre.findAll(), FilteredMovies : queryFilter});
+    if(req.session.user){
+        res.render("filter", {userData : req.session.user, genresList : await Genre.findAll(), FilteredMovies : queryFilter});
+    }
+    else{
+        res.render("filter", {genresList : await Genre.findAll(), FilteredMovies : queryFilter});
+    }
 })
 
 app.get("/search", (req,res) => {           //Chemin de recherche des films par requête GET.
-    res.render("search");
+    
+    if(req.session.user){
+        res.render("search", {userData : req.session.user});
+    }
+    else{
+        res.render("search");
+    }
 })
 
 app.post("/search", async (req, res) => {   //Chemin de recherche par requête POST.
@@ -145,7 +176,12 @@ app.post("/search", async (req, res) => {   //Chemin de recherche par requête P
             ]
         });
         console.log(query);
-        res.render("search", {result : query, research : value});
+        if(req.session.user){
+            res.render("search", {userData : req.session.user, result : query, research : value});
+        }
+        else{
+            res.render("search", {result : query, research : value});
+        }
     }
     catch{
         console.error("Erreur lors de la recherche des donneés.");
@@ -153,66 +189,196 @@ app.post("/search", async (req, res) => {   //Chemin de recherche par requête P
     }
 });
 
-app.get("/login", (req, res) => {   //Chemin de connexion.
-    res.render("login");
+app.get("/login", (req, res) => {   //Chemin de connexion initial (GET).
+
+    if(req.session.user){
+        res.render("login", {userData : req.session.user});
+    }
+    else{
+        res.render("login");
+    }
+});
+
+app.post("/login", async (req,res) => {     //Chemin de connexion (POST).
+
+    const emailInputLog = req.body.email;
+    const passwordInputLog = req.body.password;
+    let msgError = "";                  //Définition d'un message d'erreur potentiel.
+
+    //Sécuriser les entrées utilisateurs.
+    if(emailInputLog.length > 40 || passwordInputLog.length > 25){ //Gestion de la taille des entrées.
+        msgError = "La taille des champs n'a pas été respectée !";
+    }
+    else{ //Début de traitement des données.
+
+        //Gestion de l'email :
+        const safeEmail = emailInputLog.trim(); //L'input de type "email" se charge déjà de la bonne syntaxe.
+        // console.log("Email de connexion : " + safeEmail);
+
+        //Gestion du mot de passe :
+        let safePassword = funct.cleanPassword(passwordInputLog);
+        // console.log("Mot de passe de connexion : " + safePassword);
+
+        //Si le mot de passe sécurisé correspond au mot de passe initial.
+        if(safePassword == passwordInputLog){
+            //Récupérer la liste des utilisateurs.
+            const users = await User.findAll();
+            // console.log(users);
+
+            //Rechercher dans un premier temps une correspondance avec l'adresse Email.
+            let foundEmail = false;
+            let i = 0;
+
+            //Tant que l'email recherché n'est pas trouvé ou que la liste n'est pas parcouru totalement.
+            while(!foundEmail && i < users.length){
+                if(emailInputLog == users[i].user_email){ //L'email corresponds :
+                    //Email/Utilisateur trouvé.
+                    foundEmail = true;
+                }
+                else{//L'email ne corresponds pas :
+                    i++; //Passer à l'utilisateur suivant.
+                }
+            }
+
+            //Dans un second temps comparer la correspondance avec le mot de passe.
+            if(foundEmail){//L'email/l'utilisateur est trouvé : 
+                try {
+                    //await bcrypt.compare(<brutPassword>, <HashedPassword>);
+                    const isPwMatch = await bcrypt.compare(passwordInputLog, users[i].user_mdp);
+                    if (isPwMatch) {//Le mot de passe transmis corresponds.
+                        const authenticateUser = users[i];
+                        //Création de la session utilisateur.
+                        req.session.user = {
+                            username: authenticateUser.user_username,
+                            email: authenticateUser.user_email,
+                            admin: authenticateUser.user_admin,
+                        }
+                        console.log(req.session.user);
+                        console.log("Connecté avec succès !");
+                    } else {// Le mot de passe transmis ne corresponds pas.
+                        msgError = "Mot de passe incorrect.";
+                    }
+                } catch (error) {
+                    console.error('Erreur lors de la recherche de correspondance entre les mots de passe :', error);
+                }
+            }
+            else{//L'email n'est pas dans la liste des utilisateurs : 
+                msgError = "L'email transmis n'est associé à aucun compte utilisateur.";
+            }
+        }
+        else{ //Si le mot de passe initial est différent du mot de passe sécurisé :
+            //On s'arrête ici car le mot de passe entré par l'utilisateur sera différent et ne pourra pas être correctement comparé.
+            msgError = "Votre mot de passe de connexion semble suspect. Veuillez réessayer";
+        }
+    }
+    if(msgError){//Une erreur est relevée :
+        res.send(msgError);
+    }
+    else{//Succès de la connexion :
+        if(req.session.user){
+            res.render("home", {userData : req.session.user});
+        }
+        else{
+            res.render("home");
+        }
+    }
+});
+
+app.get("/logout", (req, res) => {
+
+    try {
+        req.session.destroy();          //Détruire la session utilisateur.
+        res.clearCookie("connect.sid"); //Supprimer le cookie de session du client (utilisé pour identifier).
+        console.log("Déconnecté avec Succés !");
+    } 
+    catch (error) {
+        console.log("Erreur lors de la déconnexion.");
+        res.status(500).send("Une erreur est survenue lors de la déconnexion à votre compte.");
+    }
+    res.redirect("/");  //Redirige vers l'URL de l'accueil.
 });
 
 app.get("/create-account", (req, res) => {  //Chemin de création de compte.
-    res.render("createAccount");
+    if(req.session.user){
+        res.render("createAccount", {userData : req.session.user});
+    }
+    else{
+        res.render("createAccount");
+    }
 });
 
 app.post("/create-account", async (req,res) => {    //Chemin de création de compte par POST.
+
+    //Récupérer les adresses emails existantes.
+    const existingEmail = await User.findAll({
+        attributes: ['user_email']
+    });
+    const emailList = existingEmail.map(email => email.user_email);
+    // console.log(emailList);
 
     try{
         //----------------------------------------------------
         //Voir pour mettre la partit récup + insertion dans une fonction pour séparer le code.
         //----------------------------------------------------
-        let username = req.body.username;   //Récupèration du pseudo.
-        let email = req.body.email;         //Récupération de l'email.
-        const password = req.body.password; //Récupération du mot de passe. Ne doit pas être altéré !
+
+        let usernameInputCA = req.body.username;   //Récupèration du pseudo.
+        let emailInputCA = req.body.email;         //Récupération de l'email.
+        const passwordInputCA = req.body.password; //Récupération du mot de passe. Ne doit pas être altéré !
         let msgError = "";                  //Définition d'un message d'erreur potentiel.
 
-        //Gestion du pseudo.
-        if(username.length > 25 || email.length > 40 || password.length > 25){
+        //-----------------------------------
+        //Ajouter une fonction vérif du mdp. (taille, caractère spéciaux)
+        //-----------------------------------
+
+        //Gestion de la taille des entrées.
+        if(usernameInputCA.length > 25 || emailInputCA.length > 40 || passwordInputCA.length > 25){
             msgError = "La taille des champs n'a pas été respectée !";
         }
+        //Gestion de l'unicité des adresses email. (1 compte = 1 adresse email)
+        else if(emailList.includes(emailInputCA)) {
+            msgError = "L'email transmis existe déjà. Veuillez en choisir un autre.";
+        }
+        //Début de traitement des données.
         else{
-            username = username.trim();                     //Supprimer les espaces de trop.
-            const safeUsername = sanitizeHtml(username, {   //Limiter les balises HTML :
+            usernameInputCA = usernameInputCA.trim();                     //Supprimer les espaces de trop.
+            const safeUsername = sanitizeHtml(usernameInputCA, {   //Limiter les balises HTML :
                 allowedTags: ['b','i','em','strong'],       //Balises autorisées.
                 allowedAttributes: []                       //Attributs de balises autorisés.
             });
-            console.log(safeUsername);
+            console.log("Pseudo utilisateur traité : " + safeUsername);
 
+            //Erreur en cas de nom d'utilisateur suspect.
             if(!safeUsername){
                 msgError = "Impossible de créer un compte.<br>Votre nom d'utilisateur semble suspect.";
             }
             else{
                 //Gestion de l'email :
-                const safeEmail = email.trim(); //L'input de type "email" se charge déjà de la bonne syntaxe.
-                console.log(safeEmail);
+                const safeEmail = emailInputCA.trim(); //L'input de type "email" se charge déjà de la bonne syntaxe.
+                console.log("Email traité : " + safeEmail);
 
                 //Gestion du mot de passe :
-                let safePassword = password;
-                safePassword = safePassword.trim();         //Retirer les espaces inutiles.
-                safePassword = sanitizeHtml(safePassword, { //Supprimer les caractères suspects.
-                    allowedTags: [],
-                    allowedAttributes: []
-                });
+                let safePassword = funct.cleanPassword(passwordInputCA);
 
                 //Si le mot de passe initial est différent du mot de passe sécurisé
-                if(safePassword != password){
+                if(safePassword != passwordInputCA){
                     //On empêche la création du compte car le mot de passe entré par l'utilisateur sera différent.
                     msgError = "Impossible de créer votre compte.<br>Votre mot de passe semble suspect.";
                 }
                 else{//Si le mot de passe sécurisé correspond au mot de passe initial.
                     const hashedPw = await bcrypt.hash(safePassword,11);
-                    console.log(hashedPw);
+                    console.log("Mot de passe haché : " + hashedPw);
                     
                     try{
-                        //---------------------------------------------------
                         //Insérer les informations dans la BDD.
-                        //----------------------------------------------------
+                        await User.create(
+                            {
+                                user_username : safeUsername,
+                                user_email : safeEmail,
+                                //Enregistrement du mdp haché !
+                                user_mdp : hashedPw
+                            }
+                        );
+                        console.log("Utilisateur enregistré.");
                     }
                     catch{
                         console.log("Une erreur est survenue lors de la création de votre compte utilisateur.");
@@ -221,11 +387,18 @@ app.post("/create-account", async (req,res) => {    //Chemin de création de com
                 }
             }
         }
-        if(msgError){//Si une erreur est détécte.
+        //Fin de traitement des données.
+
+        if(msgError){//Si une erreur est détécté.
             res.send(msgError);
         }
         else{//Sinon
-            res.render("login"); //Redirection vers la page de connexion.
+            if(req.session.user){
+                res.render("login", {userData : req.session.user});
+            }
+            else{
+                res.render("login"); //Redirection vers la page de connexion.
+            }
         }
     }
     catch {
