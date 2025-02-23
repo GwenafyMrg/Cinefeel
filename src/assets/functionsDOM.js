074fe171526d92183078b2889e7e16c7b5249d48
Gwenafy <gwenafy@gmail.com>
2025-02-23 09:05:19 +0100

    Documentation of the functions files.

--- src/assets/functionsDOM.js
+++ src/assets/functionsDOM.js
@@ -1,146 +1,142 @@
-//Pas d'autres solutions trouver que de recopier la fonction provenant de functions.js utiliser pour les imports de l'application node.js
-function convertRuntime(runtime){
-    //Convertit un temps en minutes exclusivement à un temps en heures et en minutes.
-    //parameters: integer
-    //return : string
+//---------- Définition des fonctions utiles au DOM : ----------//
 
-    hour = 0, min = 0;
-    min = runtime%60;
-    hour = (runtime - min)/60;
-    if(min < 10){
+function convertRuntime(runtime){
+    // Convertir un temps en minutes à un temps en heures et en minutes.
+    // parameters : 
+        // runtime : integer
+    // return : string
+
+    hour = 0, min = 0;          // Initialiser les valeurs.
+    min = runtime%60;           // Calculer les minutes.
+    hour = (runtime - min)/60;  // Calculer les heures.
+    if(min < 10){   // Si le nombre de minute est inférieur strictement à 10 :
         return `${hour}h0${min}`;
     }
-    else{
+    else{   // Sinon :
         return `${hour}h${min}`;
     }
 }
 
-//---------------------------------------A définir :-----------------------------------------
+//--------------- Gestion des éléments dynamiques du côté client : ---------------//
 
-// Gérer les boutons de retournements pour toutes les movieCards.
-document.addEventListener("DOMContentLoaded", function () {
-    // Sélectionne tous les conteneurs de cartes
+//---------- Concernant l'affichage des films : ----------//
+
+//----- Gérer les boutons de retournements pour toutes les cartes de films : -----//
+document.addEventListener("DOMContentLoaded", function () { // Au chargement de la boite.
+    // Sélectionner tous les conteneurs de cartes.
     const cardContainers = document.querySelectorAll(".card-container");
 
-    cardContainers.forEach(container => {
-        // Sélectionne tous les boutons "Retourner" à l'intérieur du conteneur
+    cardContainers.forEach(container => {   // Pour chaques conteneurs de carte :
+        // Sélectionner tous les boutons "Retourner" à l'intérieur du conteneur.
         const buttons = container.querySelectorAll(".switchButton");
 
-        buttons.forEach(button => {
-            // Ajoute un événement de clic spécifique pour ce conteneur
-            button.addEventListener("click", () => {
-                container.classList.toggle("flipped"); // Bascule uniquement ce conteneur
+        buttons.forEach(button => { // Pour chaques boutons :
+            button.addEventListener("click", () => {    // Ajouter un événement de clic spécifique.
+                container.classList.toggle("flipped");  // Donner la classe "flipped" au conteneur. 
             });
         });
     });
 });
 
-//---------------------------------------------------------------------------------------------
-//------------------------------Concernant la page des filtres :-----------------------------------
+//---------- Concernant la page des filtres : ----------//
 
-//Au chargement de la page :
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { // Au chargement de la page :
 
-    //----------------------Redirection lors de la suppression des filtres :-------------------------
-    document.getElementById("resetFilters").addEventListener("click", () => {
-        window.location.href = "/moviesList";
+    //----- Redirection lors de la suppression des filtres : -----//
+    document.getElementById("resetFilters").addEventListener("click", () => {   // Ajouter un évènement de filtre.
+        window.location.href = "/explore-movies";   // Rediriger vers la page de filtre.
     });
 
-    //----------------------Gestion de l'affichage pour la durée du film :-------------------------
+    //----- Gestion de l'affichage pour le filtre sur la durée du film : -----//
     
-    //Balises de durée Min :
-    const runtimeMinScale = document.getElementById("runtimeMinScale");     //Récupération de l'objet de valeur
-    const runtimeMinDisplay = document.getElementById("runtimeMinDisplay"); //Récupération de l'objet d'affichage
-    //Balises de durée Max :
+    // Les balises de durée Min :
+    const runtimeMinScale = document.getElementById("runtimeMinScale");     // Récupérer la balise de valeur.
+    const runtimeMinDisplay = document.getElementById("runtimeMinDisplay"); // Récupérer la balise d'affichage.
+    // Les balises de durée Max :
     const runtimeMaxScale = document.getElementById("runtimeMaxScale");
     const runtimeMaxDisplay = document.getElementById("runtimeMaxDisplay");
 
-    //---------------
-    //Utilisation de l'event "input" et non "click" pour ne pas surcharger les actions de recherche.
-    //"input" --> agit pour chaque changement de valeur.
-    //---------------
-
-    //Gestion pour la durée minimale :
-    runtimeMinDisplay.textContent = convertRuntime(runtimeMinScale.value);  //Initialisation de l'affichage.
-    runtimeMinScale.addEventListener("input", function() {                  //Pour chaque changement.                //Récupération de la valeur actuelle
-        runtimeMinDisplay.textContent = convertRuntime(runtimeMinScale.value);    //Actualisation de l'affchage
+    // Gérer la durée minimale :
+    runtimeMinDisplay.textContent = convertRuntime(runtimeMinScale.value);  // Initialiser l'affichage.
+    runtimeMinScale.addEventListener("input", function() {                  // Ajouter un évènement de modification.
+        runtimeMinDisplay.textContent = convertRuntime(runtimeMinScale.value);    // Actualiser l'affichage.
     });
 
-    //Gestion pour la durée maximale.
-    runtimeMaxDisplay.textContent = convertRuntime(runtimeMaxScale.value);
-    runtimeMaxScale.addEventListener("input", function() {
-        runtimeMaxDisplay.textContent = convertRuntime(runtimeMaxScale.value);
+    // Gérer la durée maximale :
+    runtimeMaxDisplay.textContent = convertRuntime(runtimeMaxScale.value);  // Initialiser l'affichage.
+    runtimeMaxScale.addEventListener("input", function() {                  // Ajouter un évènement de modification.
+        runtimeMaxDisplay.textContent = convertRuntime(runtimeMaxScale.value);  // Actualiser l'affichage.
     });
 
-    //----------------------Année de sortie du film à filtrer :-------------------------
+    //----- Gestion de l'affichage pour le filtre sur l'année de sortie du film : -----//
 
-    //Balise de date de sortie Min:
+    // Les balises de date de sortie minimale :
     dateMinScale = document.getElementById("dateMinScale");
     dateMinDisplay = document.getElementById("dateMinDisplay");
-    //Balise de date de sortie Max:
+    // Les balises de date de sortie maximale :
     dateMaxScale = document.getElementById("dateMaxScale");
     dateMaxDisplay = document.getElementById("dateMaxDisplay");
 
-    //Gestion pour la date de sortie minimale :
+    // Gérer la date de sortie minimale :
     dateMinDisplay.textContent = dateMinScale.value;
     dateMinScale.addEventListener("input", function() {
         dateMinDisplay.textContent = dateMinScale.value;
     });
-    //Gestion pour la date de sortie maximale :
+    // Gérer la date de sortie maximale :
     dateMaxDisplay.textContent = dateMaxScale.value;
     dateMaxScale.addEventListener("input", function() {
         dateMaxDisplay.textContent = dateMaxScale.value;
     });   
 
-    //----------------------Note du film à filtrer :-------------------------
+    //----- Gestion de l'affichage pour le filtre de la note du film : -----//
 
-    //Balise de note minimale:
+    // Les balises de note minimale :
     noteMinScale = document.getElementById("noteMinScale");
     noteMinDisplay = document.getElementById("noteMinDisplay");
-    //Balise de note maximale:
+    // Les balises de note maximale :
     noteMaxScale = document.getElementById("noteMaxScale");
     noteMaxDisplay = document.getElementById("noteMaxDisplay");
 
-    //Actualisation de la note minimale :
+    // Gérer la note minimale :
     noteMinDisplay.textContent = noteMinScale.value;
     noteMinScale.addEventListener("input", function() {
         noteMinDisplay.textContent = noteMinScale.value;
     });
-    //Actualisation de la note maximale :
+    // Gérer la note maximale :
     noteMaxDisplay.textContent = noteMaxScale.value;
     noteMaxScale.addEventListener("input", function() {
         noteMaxDisplay.textContent = noteMaxScale.value;
     });
 });
 
-//---------------------------------------------------------------------------------------------
-//------------------------------Concernant la page des avis :-----------------------------------
+//---------- Concernant la page des avis : ----------//
 
-document.addEventListener("DOMContentLoaded", function() {      //Au chargement de la page :
+document.addEventListener("DOMContentLoaded", function() {      // Au chargement de la page :
 
-    //Actualiser la note choisie :
+    //----- Gestion de l'affichage de la note choisie pour le film : -----//
     const noteDisplay = document.getElementById("noteDisplay");
     const noteInput = document.getElementById("noteInput");
-    noteDisplay.textContent = noteInput.value;      //Initialisation à la valeur par défaut du input :
+    noteDisplay.textContent = noteInput.value;      // Initialiser la valeur par défaut du input :
 
     noteInput.addEventListener("input", function() {
-        noteDisplay.textContent = noteInput.value;  //Actualisation à chaque changement de l'input.
+        noteDisplay.textContent = noteInput.value;  // Actualiser à chaque changement.
     });
 
-    // Sélection de la <div> contenant les checkboxes
+    //----- Gestion de l'affichage des émotions pour le film : -----//
+    // Séléctionner le container contenant les checkboxes.
     const emotionsContainer = document.getElementById('emotionsContainer');
 
-    // Ajout d'un gestionnaire d'événements pour surveiller les changements sur les checkboxes
+    // Ajout d'un gestionnaire d'événements pour surveiller les changements sur les checkboxes :
     emotionsContainer.addEventListener('change', (event) => {
-        //Récupérer toutes les cases pouvant être cochées pour les émotions.
+        // Récupérer toutes les cases pouvant être cochées pour les émotions.
         const emotionsCheckboxes = emotionsContainer.querySelectorAll('input[type="checkbox"]');
-        //Récupérer uniquement les cases cochées.
+        // Récupérer uniquement les cases cochées.
         const checkedCheckboxes = Array.from(emotionsCheckboxes).filter(checkbox => checkbox.checked);
 
-        // Si plus de 3 checkboxes sont cochées, décocher la case qui vient d'être cochée
+        // Si il y a plus de 3 checkboxes cochées : 
         if (checkedCheckboxes.length > 3) {
-            event.target.checked = false;
-            Swal.fire({
+            event.target.checked = false;   // Décocher la case qui vient d'être cochée.
+            Swal.fire({                     // Afficher un pop-up.
                 title: "Attention !",
                 text: "Vous ne pouvez voter que pour 3 émotions au maximum.",
                 icon : "error",
@@ -149,14 +145,16 @@ document.addEventListener("DOMContentLoaded", function() {      //Au chargement 
         }
     });
 
-    const commentInput = document.getElementById("commentInput");
+    //----- Gestion du nombre de caractère maximale pour le commentaire du film : -----//
+    const commentInput = document.getElementById("commentInput");   // Récupérer la balise de texte.
     // console.log(commentInput);
-    if(commentInput){
-        commentInput.addEventListener('input', function() {
-            console.log(commentInput.value.length);
-            if(commentInput.value.length > 500){
-                commentInput.value = commentInput.value.substring(0,500);
-                Swal.fire({
+
+    if(commentInput){   // Si la balise de texte a bien été récupéré :
+        commentInput.addEventListener('input', function() { // Ajouter un écouteur d'évènement :
+            // console.log(commentInput.value.length);
+            if(commentInput.value.length > 500){    // Si le texte fait plus de 500 caractères :
+                commentInput.value = commentInput.value.substring(0,500);   // Conserver uniquement les 500 premiers caractères.
+                Swal.fire({     // Afficher un pop-up pour informer l'utilisateur du dépassement.
                     title: "Attention !",
                     text: "Vous ne pouvez rédiger un commentaire de plus de 500 caractères.",
                     icon : "error",
